ac79e63bbe9b82bf611523cb5d49dc21de392d16
Howler4695 <30935200+Howler4695@users.noreply.github.com>
2018-10-10 21:00:17 -0500

    Update PingODeath.bat

--- PingODeath.bat
+++ PingODeath.bat
@@ -1,5 +1,14 @@
 @echo off
 
+echo An archaic batch file I made way back in the day.
+echo If you find an ip address to direct it to it will
+echo start a bunch of processes, very crudely, and start pinging the ip; 
+echo Acting as a DOS attack. 
+echo I would use this on our schools routers that
+echo for some reason would take and process every ping I gave it. 
+echo I tried it on my old router, it is no longer functional. 
+echo WILL MAKE YOUR COMPUTER HAVE A SEIZURE.
+
 set /p numAttack="Pings per process: "
 set /p numProcess="Number of ping processes: "
 set /p sizeByte="Bytes per ping: "
